3bcb502076f29c30299c0ddda1d495556f40e71a
Mark Druffel <mark.druffel@gmail.com>
2025-04-24 11:20:31 -0700

    Update script for stand alone

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
 sudo apt update
 REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
@@ -17,9 +21,14 @@ curl -sS https://starship.rs/install.sh | sh
 eval "$(starship init zsh)"
 
 # Install font
-curl -Lo ~/Downloads/VictorMono.zip https://github.com/ryanoasis/nerd-fonts/releases/latest/download/VictorMono.zip
-unzip ~/Downloads/VictorMono.zip 
-sudo mv ~/Downloads/VictorMono/* /~/.local/share/fonts
+FONT_ZIP="$HOME/Downloads/VictorMono.zip"
+FONT_DIR="$HOME/.local/share/fonts"
+mkdir -p "$FONT_DIR"
+curl -Lo "$FONT_ZIP" https://github.com/ryanoasis/nerd-fonts/releases/latest/download/VictorMono.zip
+unzip -o "$FONT_ZIP" -d "$FONT_DIR"
+fc-cache -fv
+
+# Set GNOME terminal font
 PROFILE_ID=$(dconf list /org/gnome/terminal/legacy/profiles:/ | head -n 1 | tr -d '/')
 dconf write /org/gnome/terminal/legacy/profiles:/$PROFILE_ID/font "'VictorMono Nerd Font 10'"
 dconf write /org/gnome/terminal/legacy/profiles:/$PROFILE_ID/use-system-font false
